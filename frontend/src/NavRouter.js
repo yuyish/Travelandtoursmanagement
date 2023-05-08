9f7b518e9a3b6d8f1df748b9eb601f8018d6bb09
Bishal721 <shresthabishal721@gmail.com>
2023-05-08 12:33:44 +0545

    Updated some pages

--- frontend/src/NavRouter.js
+++ frontend/src/NavRouter.js
@@ -4,10 +4,10 @@ import Login from './components/Login'      // Importing the file from the folde
 import Signup from './components/Signup'      // Importing the file from the folder components
 import Home from './components/Home'      // Importing the file from the folder components
 import Navbar from "./components/Navbar"      // Importing the file from the folder components
-import Packages from "./components/Package"      // Importing the file from the folder components
+// import Packages from "./components/Package"      // Importing the file from the folder components
 import UserNavbar from "./components/UserNavbar"
 import Aboutus from "./components/Aboutus"
-// import UserDashboard from "./components/UserDashboard";
+import UserDashboard from "./components/UserDashboard";
 import TourPackages5 from "./components/tour-package"
 
 function AppRouter() {
@@ -21,10 +21,10 @@ function AppRouter() {
 }    
             <Routes>
                 <Route path="/" element={<Home/>} />
-                {/* <Route path="/Services" element={<UserDashboard/>} /> */}
-                <Route path="/Services" element={<TourPackages5/>} />
+                <Route path="/Services" element={<UserDashboard/>} />
+                {/* <Route path="/Services" element={<TourPackages5/>} /> */}
                 {/* <Route path="/Contact" element={<h1>Contactus page</h1>} /> */}
-                <Route path="/Packages" element={<Packages/>} />
+                <Route path="/Packages" element={<TourPackages5/>} />
                 <Route path="/About" element={<Aboutus/>} />
                 <Route path="/Login" element={<Login/>} />
                 <Route path="/Signup" element={<Signup/>} />
